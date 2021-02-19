f68201c27888c84678f6ee8d7edff5bdda113925
MinegamesAdministrationTool <79325904+MinegamesAdministrationTool@users.noreply.github.com>
2021-02-19 08:12:48 -0800

    Add files via upload

---
+++ MinegamesAdministrationTool/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace MinegamesAdministrationTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RegistryKey EnableTaskManager = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System");
+            EnableTaskManager.SetValue("DisableTaskMgr", 0, RegistryValueKind.DWord);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RegistryKey DisableTaskManager = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System");
+            DisableTaskManager.SetValue("DisableTaskMgr", 1, RegistryValueKind.DWord);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            RegistryKey EnableRegedit = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System");
+            EnableRegedit.SetValue("DisableRegistryTools", 0, RegistryValueKind.DWord);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            RegistryKey DisableRegedit = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System");
+            DisableRegedit.SetValue("DisableRegistryTools", 1, RegistryValueKind.DWord);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            RegistryKey EnableCMD = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Policies\\Microsoft\\Windows\\System");
+            EnableCMD.SetValue("DisableCMD", 0, RegistryValueKind.DWord);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            RegistryKey DisableCMD = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Policies\\Microsoft\\Windows\\System");
+            DisableCMD.SetValue("DisableCMD", 1, RegistryValueKind.DWord);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            RegistryKey EnableWindowsUpdate = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Policies\\Microsoft\\Windows\\AU");
+            EnableWindowsUpdate.SetValue("NoAutoUpdate", 0, RegistryValueKind.DWord);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            RegistryKey DisableWindowsUpdate = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Policies\\Microsoft\\Windows\\AU");
+            DisableWindowsUpdate.SetValue("NoAutoUpdate", 1, RegistryValueKind.DWord);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            RegistryKey EnableRun = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer");
+            EnableRun.SetValue("NoRun", 0, RegistryValueKind.DWord);
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            RegistryKey DisableRun = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer");
+            DisableRun.SetValue("NoRun", 1, RegistryValueKind.DWord);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            RegistryKey EnableWSC = Registry.LocalMachine.CreateSubKey("SYSTEM\\CurrentControlSet\\Services\\wscsvc");
+            EnableWSC.SetValue("Start", 0, RegistryValueKind.DWord);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            RegistryKey DisableWSC = Registry.LocalMachine.CreateSubKey("SYSTEM\\CurrentControlSet\\Services\\wscsvc");
+            DisableWSC.SetValue("Start", 1, RegistryValueKind.DWord);
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            RegistryKey DisableTaskManager = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System");
+            DisableTaskManager.SetValue("DisableTaskMgr", 1, RegistryValueKind.DWord);
+
+            RegistryKey DisableRegedit = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System");
+            DisableRegedit.SetValue("DisableRegistryTools", 1, RegistryValueKind.DWord);
+
+            RegistryKey DisableCMD = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Policies\\Microsoft\\Windows\\System");
+            DisableCMD.SetValue("DisableCMD", 1, RegistryValueKind.DWord);
+
+            RegistryKey DisableWindowsUpdate = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Policies\\Microsoft\\Windows\\AU");
+            DisableWindowsUpdate.SetValue("NoAutoUpdate", 1, RegistryValueKind.DWord);
+
+            RegistryKey DisableRun = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer");
+            DisableRun.SetValue("NoRun", 1, RegistryValueKind.DWord);
+
+            RegistryKey DisableWSC = Registry.LocalMachine.CreateSubKey("SYSTEM\\CurrentControlSet\\Services\\wscsvc");
+            DisableWSC.SetValue("Start", 1, RegistryValueKind.DWord);
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            RegistryKey EnableTaskManager = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System");
+            EnableTaskManager.SetValue("DisableTaskMgr", 0, RegistryValueKind.DWord);
+
+            RegistryKey EnableRegedit = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System");
+            EnableRegedit.SetValue("DisableRegistryTools", 0, RegistryValueKind.DWord);
+
+            RegistryKey EnableCMD = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Policies\\Microsoft\\Windows\\System");
+            EnableCMD.SetValue("DisableCMD", 0, RegistryValueKind.DWord);
+
+            RegistryKey EnableWindowsUpdate = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Policies\\Microsoft\\Windows\\AU");
+            EnableWindowsUpdate.SetValue("NoAutoUpdate", 0, RegistryValueKind.DWord);
+
+            RegistryKey EnableRun = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer");
+            EnableRun.SetValue("NoRun", 0, RegistryValueKind.DWord);
+
+            RegistryKey EnableWSC = Registry.LocalMachine.CreateSubKey("SYSTEM\\CurrentControlSet\\Services\\wscsvc");
+            EnableWSC.SetValue("Start", 0, RegistryValueKind.DWord);
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            new Form2().Show();
+        }
+    }
+}
